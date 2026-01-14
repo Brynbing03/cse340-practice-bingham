c4f9820a8c225fc9fede182e586c9ad7c4adbc20
Brynlee Bingham <[Brynbing03]@users.noreply.github.com>
2026-01-14 14:42:10 -0700

    EJS templates with partials

--- server.js
+++ server.js
@@ -1,14 +1,52 @@
-import express from 'express';     
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
 
-const app = express();               
-const name = process.env.NAME;       
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send(`Welcome, ${name}!`);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+const NODE_ENV = process.env.NODE_ENV || "production";
+const PORT = process.env.PORT || 3000;
+
+// middleware
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
+
+// routes...
+
+app.get("/", (req, res) => {
+  res.render("home", { title: "Welcome Home" });
 });
 
-const PORT = 3000;
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About Me" });
+});
 
-app.listen(PORT, () => {   
-  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+app.get("/products", (req, res) => {
+  res.render("products", { title: "Our Products" });
+});
+
+app.get("/student", (req, res) => {
+  res.render("student", {
+    title: "Student Information",
+    name: "Brynlee Bingham",
+    id: "A01234567",
+    email: "brynlee@example.com",
+    address: "Rexburg, ID",
+  });
+});
+
+// server stuff
+
+app.listen(PORT, () => {
+  console.log(`Running in ${NODE_ENV} mode`);
+  console.log(`Server running at http://127.0.0.1:${PORT}`);
 });
