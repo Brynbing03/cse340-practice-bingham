226af2a64e40da298b4e0787bd5450b0c61cdfce
Brynlee Bingham <[Brynbing03]@users.noreply.github.com>
2026-01-14 14:53:50 -0700

    doing the intro to routing assignment and messed it up a little bit so i have 2 seperate branches, haha it is kinda confusing but i think i got it to work. i also was working on the template engine assignment

--- server.js
+++ server.js
@@ -10,43 +10,28 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-const NODE_ENV = process.env.NODE_ENV || "production";
 const PORT = process.env.PORT || 3000;
 
 // middleware
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "src/views"));
-
-// routes...
+// routes
 
 app.get("/", (req, res) => {
-  res.render("home", { title: "Welcome Home" });
+  res.sendFile(path.join(__dirname, "src/views/home.html"));
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", { title: "About Me" });
+  res.sendFile(path.join(__dirname, "src/views/about.html"));
 });
 
 app.get("/products", (req, res) => {
-  res.render("products", { title: "Our Products" });
-});
-
-app.get("/student", (req, res) => {
-  res.render("student", {
-    title: "Student Information",
-    name: "Brynlee Bingham",
-    id: "A01234567",
-    email: "brynlee@example.com",
-    address: "Rexburg, ID",
-  });
+  res.sendFile(path.join(__dirname, "src/views/products.html"));
 });
 
 // server stuff
 
 app.listen(PORT, () => {
-  console.log(`Running in ${NODE_ENV} mode`);
   console.log(`Server running at http://127.0.0.1:${PORT}`);
 });
